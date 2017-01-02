899c066a6721cb1ea1916d7aa6a6437c04242bab
e_lar <elarb@users.noreply.github.com>
2017-01-02 22:41:37 +0100

    Changed table 'Users' and added 'todoItems'

--- migrations/20160315161907_users.js
+++ migrations/20160315161907_users.js
@@ -1,29 +1,37 @@
 exports.up = function (knex, Promise) {
     return Promise.all([
-        knex.schema.createTable('users', function (table) {
+        knex.schema.createTable('Users', function (table) {
                 table.increments();
-                table.string('username').unique();
-                table.string('email');
-                table.string('name');
-                table.string('password');
-                table.string('passwordResetToken');
-                table.dateTime('passwordResetExpires');
-                table.string('gender');
-                table.string('country');
-                table.string('city');
-                table.string('picture');
-                table.string('facebook');
-                table.string('twitter');
-                table.string('google');
+            table.string('Username').unique();
+            table.string('Email');
+            table.string('Name');
+            table.string('Password');
+            table.string('PasswordResetToken');
+            table.dateTime('PasswordResetExpires');
+            table.string('Gender');
+            table.string('Country');
+            table.string('Region');
+            table.string('Picture');
+            table.string('Facebook');
+            table.string('Twitter');
+            table.string('Google');
                 table.timestamps();
             }
         ),
-        knex.schema.createTable('todolist', function (table) {
-
-            }
-        ),
-        knex.schema.createTable('todoitem', function (table) {
-
+        // knex.schema.createTable('todolist', function (table) {
+        //
+        //     }
+        // ),
+        knex.schema.createTable('TodoItems', function (table) {
+            table.increments();
+            table.string('Title');
+            table.string('Description');
+            table.date('DueDate');
+            table.integer('Priority');
+            table.boolean('Completed');
+            table.date('CompletionDate');
+            table.timestamps();
+            table.integer('UserID').unsigned().index().references('id').inTable('Users');
             }
         )
     ]);
