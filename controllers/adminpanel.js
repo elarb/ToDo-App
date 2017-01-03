9e7d698cdbb18848a3f0b80a9375c85a92cb89a2
e_lar <elarb@users.noreply.github.com>
2017-01-03 14:57:54 +0100

    added admin-panel

--- controllers/adminpanel.js
+++ controllers/adminpanel.js
@@ -1,3 +1,19 @@
+const mysql = require('mysql');
+const User = require('../models/User');
+const chalk = require('chalk');
+
 /**
- * Created by Elias on 03-Jan-17.
+ * Connect to MySQL.
  */
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+connection.connect();
+
+connection.on('error', () => {
+    console.log('%s MySQL connection error. Please make sure MySQL is running.', chalk.red('✗'));
+    process.exit();
+});
