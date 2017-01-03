8e8e95b547b414b8372d234276a156eeaf2944fe
e_lar <elarb@users.noreply.github.com>
2017-01-04 00:17:11 +0100

    temp fix

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -25,10 +25,8 @@ exports.getFeedback = (req, res) => {
  * Send a feedback form via Nodemailer.
  */
 exports.postFeedback = (req, res) => {
-    if (!req.user) {
-        req.assert('name', 'Name cannot be blank').notEmpty();
-        req.assert('email', 'Email is not valid').isEmail();
-    }
+    req.assert('name', 'Name cannot be blank').notEmpty();
+    req.assert('email', 'Email is not valid').isEmail();
     req.assert('message', 'Message cannot be blank').notEmpty();
 
     const errors = req.validationErrors();
@@ -38,60 +36,60 @@ exports.postFeedback = (req, res) => {
         return res.redirect('/feedback');
     }
 
-    if (req.user) {
-        async.waterfall([
-            function (done) {
-                new User({id: req.user.id})
-                    .fetch()
-                    .then(function (user) {
-                        done(user.toJSON());
-                    });
-            },
-            function (user, done) {
-                const transporter = nodemailer.createTransport({
-                    service: 'SendGrid',
-                    auth: {
-                        user: process.env.SENDGRID_USER,
-                        pass: process.env.SENDGRID_PASSWORD
-                    }
-                });
-                const mailOptions = {
-                    to: process.env.PERSONAL_MAIL,
-                    from: `${user.Name} <${user.Email}>`,
-                    subject: 'Feedback Form | To Do App',
-                    text: req.body.message
-                };
-                transporter.sendMail(mailOptions, (err) => {
-                    if (err) {
-                        req.flash('errors', {msg: err.message});
-                        return res.redirect('/feedback');
-                    }
-                    req.flash('success', {msg: 'Email has been sent successfully!'});
-                    done(err);
-                });
-            }, (err) => {
-                if (err) {
-                    return next(err);
-                }
-                res.redirect('/');
-            }
-        ])
-    } else {
-        const mailOptions = {
-            to: process.env.PERSONAL_MAIL,
-            from: `${req.body.name} <${req.body.email}>`,
-            subject: 'Feedback Form | To Do App',
-            text: req.body.message
-        };
+    // if (req.user) {
+    //     async.waterfall([
+    //         function (done) {
+    //             new User({id: req.user.id})
+    //                 .fetch()
+    //                 .then(function (user) {
+    //                     done(user.toJSON());
+    //                 });
+    //         },
+    //         function (user, done) {
+    //             const transporter = nodemailer.createTransport({
+    //                 service: 'SendGrid',
+    //                 auth: {
+    //                     user: process.env.SENDGRID_USER,
+    //                     pass: process.env.SENDGRID_PASSWORD
+    //                 }
+    //             });
+    //             const mailOptions = {
+    //                 to: process.env.PERSONAL_MAIL,
+    //                 from: `${user.Name} <${user.Email}>`,
+    //                 subject: 'Feedback Form | To Do App',
+    //                 text: req.body.message
+    //             };
+    //             transporter.sendMail(mailOptions, (err) => {
+    //                 if (err) {
+    //                     req.flash('errors', {msg: err.message});
+    //                     return res.redirect('/feedback');
+    //                 }
+    //                 req.flash('success', {msg: 'Email has been sent successfully!'});
+    //                 done(err);
+    //             });
+    //         }, (err) => {
+    //             if (err) {
+    //                 return next(err);
+    //             }
+    //             res.redirect('/');
+    //         }
+    //     ])
+    // } else {
+    const mailOptions = {
+        to: process.env.PERSONAL_MAIL,
+        from: `${req.body.name} <${req.body.email}>`,
+        subject: 'Feedback Form | To Do App',
+        text: req.body.message
+    };
 
-        transporter.sendMail(mailOptions, (err) => {
-            if (err) {
-                req.flash('errors', {msg: err.message});
-                return res.redirect('/feedback');
-            }
-            req.flash('success', {msg: 'Email has been sent successfully!'});
-            res.redirect('/');
-        });
-    }
+    transporter.sendMail(mailOptions, (err) => {
+        if (err) {
+            req.flash('errors', {msg: err.message});
+            return res.redirect('/feedback');
+        }
+        req.flash('success', {msg: 'Email has been sent successfully!'});
+        res.redirect('/');
+    });
+
 };
 
