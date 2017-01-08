b9a53ae75445d1fc8ab18057cc32807351846a6f
e_lar <elarb@users.noreply.github.com>
2017-01-08 13:37:44 +0100

    disabled admin panel connection

--- controllers/adminpanel.js
+++ controllers/adminpanel.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql');
-const User = require('../models/User');
-const chalk = require('chalk');
+// const mysql = require('mysql');
+// const User = require('../models/User');
+// const chalk = require('chalk');
 
 /**
  * Connect to MySQL.
  */
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-connection.connect();
-
-connection.on('error', () => {
-    console.log('%s MySQL connection error. Please make sure MySQL is running.', chalk.red('✗'));
-    process.exit();
-});
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+// connection.connect();
+//
+// connection.on('error', () => {
+//     console.log('%s MySQL connection error. Please make sure MySQL is running.', chalk.red('✗'));
+//     process.exit();
+// });
